3939eba4861a3d515bbb4a464606594eab62d711
fenglei7858 <fenglei7858@gmail.com>
2016-11-16 20:41:12 -0500

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,23 +1,6 @@
-/*
- * Write a program that iterates through numbers from 0 to 113 using a loop. Print the numbers, one
-number per line. As you print each number, say x, also print the following when appropriate,
-separated by commas:
-I. If the number is odd, print “x is odd”
-II. If the number is divisible by 5, print “hi five”
-III. If the total of a number (x) and its subsequent number (x+1) is a value divisible by 7, print “wow”
-IV. If the number is prime, print “prime”.
- */
 
 /**
-* Assignment1/Q9, COMP268
-* Class:Number.java
-* Purpose: Write a program that can recognize 4 conditions and print what it is.
-* @author: Lei Feng
-* Student ID: 3294132
-* Date: July 19, 2016
-* Version 1.0
-* based on :David J. Eck, Hobart and William Smith Colleges(Version 6.0, June 2011),
-* Introduction to Programming Using Java
+*@author Lei Feng
 */
 import java.util.ArrayList;
 
