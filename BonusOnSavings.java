cc5732cd25fa145bcf6f697d1d964edfbf2cfc6c
fenglei7858 <fenglei7858@gmail.com>
2016-11-16 20:40:01 -0500

    Update BonusOnSavings.java

--- BonusOnSavings.java
+++ BonusOnSavings.java
@@ -1,26 +1,5 @@
-/*
-* Space Inc. will give a quarterly and annual bonus to its employees only if the savings of the quarter
-* and/or the year are greater than or equal to quarterly minimum (monthly commitment  x 3) and/or
-* the annual minimum (monthly commitment  x 12) amount, respectively. The quarterly bonus is 3%
-* of eligible quarterly savings, and the annual bonus is 5% of annual savings if eligible. If the annual
-* savings exceeds the committed amount by at least 25%, Space Inc. matches the additional savings
-* (25% or above) as part of the annual bonus.
-* I. An employee has committed to save $2000 per month. Her quarterly savings are as follows:
-* Q1 – $5000, Q2 – $7000, Q3 – $4000, and Q4 – $8000.
-* II. Another employee has committed to save $3000 per month. His quarterly savings are as follows:
-* Q1 – $6000, Q2 – $9000, Q3 – $10000, and Q4 – $17000.
-* Write a program to compute the total bonus amount earned by these two employees in the year.
- */
 /**
-* Assignment1/Q2, COMP268
-* Class: BonuesOnSavings.java
-* Purpose: to compute the total bonus amount earned by two given employees in the year.
-* @author: Lei Feng
-* Student ID: 3294132
-* Date: July 19, 2016
-* Version 1.0
-* based on :David J. Eck, Hobart and William Smith Colleges(Version 6.0, June 2011),
-* Introduction to Programming Using Java
+*@author Lei Feng
 */
 
 import java.util.Arrays;
